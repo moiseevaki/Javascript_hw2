91d513a51f6a73000f9b62f455079ba05ce533b7
Ksenia Moiseeva <moiseeva.k.i@gmail.com>
2016-05-14 23:04:15 +0300

    fixed hw

--- script.js
+++ script.js
@@ -6,22 +6,39 @@
 //Если нет совпадения, то есть введенное имя пользователя не существует в массиве - выдавать с помощью alert сообщение об ошибке
 //Если есть совпадение - выводить сообщение "Андрей, вы успешно вошли". Вместо "Андрей" должно быть имя текущего пользователя
 
-var arrName = []; 
-var login;
+var login = (function() {
+    var names = [],
+        user;
 
-for (var i = 0; i < 5; i++) { 
-	arrName[i] = prompt('Введите ' + (i + 1) + '-ое имя'); 
-} 
+    var init = function() {
+        var i = 0;
 
-console.log('Авторизированные пользователи: ', arrName);
+        for (i; i < 5; i++) {
+            names.push( prompt('Введите имя') );
+        }
 
-login = prompt('Введите имя пользователя'); 
+    };
 
-for (var i = 0; i < arrName.length; i++) { 
-	if (arrName[i] == login) { 
-	alert(login + ', Вы успешно вошли!'); 
-	break; 
-	} else if ((i == arrName.length - 1) && (arrName[i] != login)) { 
-	alert('Логин не найден'); 
-	} 
-}
+    var welcome = function() {
+        user = prompt('Введите имя пользователя');
+    };
+
+    var check = function() {
+        if(names.indexOf(user) > -1) {
+            console.log(user +  ', вы успешно вошли');
+        } else {
+            console.log('Ошибка');
+        }
+    };
+
+    return {
+        init: init,
+        welcome: welcome,
+        check: check
+    }
+
+})();
+
+login.init();
+login.welcome();
+login.check();
